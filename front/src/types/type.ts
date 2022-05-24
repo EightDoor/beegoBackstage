a859186bd05013a000bf6711f237ae56891caaef
EightDoor <zhou851708184@163.com>
2022-05-24 23:19:41 +0800

    chore(front): fat-crud依赖升级，打包错误解决

--- front/src/types/type.ts
+++ front/src/types/type.ts
@@ -1,66 +1,64 @@
-import { ColumnProps } from "ant-design-vue/lib/table/interface";
-
 // 请求接口分页
 export interface PaginType {
-  pageNum?: number;
-  pageSize?: number;
-  total?: number;
+  pageNum?: number
+  pageSize?: number
+  total?: number
 }
 
 interface CommReqList<T> extends PaginType {
-  list: T[];
+  list: T[]
 }
 export interface CommonResponse<T> {
-  list: CommReqList<T> | null;
-  data: any;
+  list: CommReqList<T> | null
+  data: any
 }
 
 // 表格默认的分页
 export interface TablePaginType {
-  current: number;
-  pageSize: number;
-  total: number;
+  current: number
+  pageSize: number
+  total: number
 }
 
 export interface CommonResponseSing<T> extends PaginType {
-  list: T;
+  list: T
 }
 
 interface AntColumnSlot {
   bodyCell?: {
-    customRender: string;
-  };
-  title: string;
-  dataIndex?: string;
-  key?: string;
-  fixed?: string | boolean;
-  width?: number;
+    customRender: string
+  }
+  title: string
+  dataIndex?: string
+  key?: string
+  fixed?: string | boolean
+  width?: number
 }
 
-interface AntColumn extends ColumnProps, AntColumnSlot {}
+interface AntColumn extends AntColumnSlot {}
 
 // 表格
 export interface TableDataType<T> extends PaginType {
-  data: T[];
-  columns: AntColumn[];
-  loading: boolean;
+  data: T[]
+  columns: AntColumn[]
+  loading: boolean
 }
 
 // 通用的业务类型字段
 export interface CommonTableList {
-  id?: number;
-  createdAt?: string;
-  updatedAt?: string;
+  id?: number
+  createdAt?: string
+  updatedAt?: string
 }
 
 // 树形tree-select
 export interface CommonTreeSelect {
-  title?: string;
-  value?: string;
-  key?: string;
+  title?: string
+  value?: string
+  key?: string
 }
 // 树形结构选中selectKey
 export interface CommonTreeSelectKeys {
-  checked: string[];
-  halfChecked: string[];
+  checked: string[]
+  halfChecked: string[]
 }
